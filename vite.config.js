c7972b1eadc1621ff87957caf30af6c9aa957259
ErikRichter1201 <161591205+ErikRichter1201@users.noreply.github.com>
2025-01-07 17:14:39 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     // Nutzt das React SWC Plugin, um JSX korrekt zu verarbeiten
-    react()
+    react(),
   ],
   define: {
     'process.env': process.env, // Setzt Umgebungsvariablen in Vite
@@ -14,4 +14,8 @@ export default defineConfig({
     // Stellt sicher, dass Vite `.jsx` Dateien korrekt auflöst
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-})
+  server: {
+    host: true, // Öffnet den Server für alle Netzwerke (z. B. 0.0.0.0)
+    port: 5173, // Setzt den Port, falls du einen anderen bevorzugst, passe ihn an
+  },
+});
